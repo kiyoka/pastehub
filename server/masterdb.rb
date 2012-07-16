3ab093d50731cd8270c83f37a2035c921c4d472b
Kiyoka Nishiyama <kiyoka@sumibi.org>
2012-07-16 04:45:41 +0000

    Remove space

--- server/masterdb.rb
+++ server/masterdb.rb
@@ -45,7 +45,7 @@ masterdb_server.request_handler do |req|
     if ret[0]
       username = ret[1]
       puts "Connected from user [#{username}]"
-    else 
+    else
       puts "Error: " + ret[1].to_s
       req.response.status_code = 403
       req.response.status_message = "Authorization failure."
