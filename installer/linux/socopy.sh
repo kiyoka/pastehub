871bdd0c40bd423024c9e04c51812832f1ad6fef
Kiyoka Nishiyama <kiyoka@sumibi.org>
2012-09-18 22:41:51 +0900

    Add PastehubSync script. Update so copy script.

--- installer/linux/socopy.sh
+++ installer/linux/socopy.sh
@@ -7,8 +7,19 @@ RUBY=ruby-1.9.3-p194
 TARGET=/opt/pastehub/lib
 
 #ldd ./work/${RUBY}/bin/ruby
-solist='/lib/libpthread.so.0 /lib/librt.so.1 /lib/libdl.so.2 /lib/libcrypt.so.1 /lib/libm.so.6 /lib/libc.so.6 /lib/libfreebl3.so'
-
+solist='
+/lib/libcom_err.so.2
+/lib/libcrypto.so.10
+/lib/libgssapi_krb5.so.2
+/lib/libk5crypto.so.3
+/lib/libkeyutils.so.1
+/lib/libkrb5.so.3
+/lib/libkrb5support.so.0
+/lib/libselinux.so.1
+/lib/libssl.so.10
+/lib/libz.so.1
+/usr/lib/libssl.so.10
+/usr/lib/libgdbm.so.2'
 
 mkdir -p ${TARGET}
 for i in ${solist} ;
@@ -16,7 +27,3 @@ do
   echo "  installed [" ${i} "]"
   /bin/cp -f ${i} ${TARGET}
 done
-
-
-
-
