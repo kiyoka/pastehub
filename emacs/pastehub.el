31736fdff0db98e19216dee799e24e858bec4bac
Kiyoka Nishiyama <kiyoka@sumibi.org>
2012-12-22 11:47:21 +0900

    Assign license to pastehub.el as GPLv2.

--- emacs/pastehub.el
+++ emacs/pastehub.el
@@ -4,6 +4,23 @@
 ;;
 ;;   Copyright (C) 2012 Kiyoka Nishiyama
 ;;
+;;
+;; This file is part of PasteHub.el
+;;
+;; PasteHub.el is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+;; 
+;; PasteHub.el is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;; 
+;; You should have received a copy of the GNU General Public License
+;; along with PasteHub.el; see the file COPYING.
+;;
+
 
 (defcustom pastehub-sync-items 10
   "number of paste items to sync."
