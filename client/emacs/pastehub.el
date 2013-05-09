f9c355ba5f0c9454269bcf04cd9274075e2c0437
Kiyoka Nishiyama <kiyoka@sumibi.org>
2013-05-10 00:30:52 +0900

    Update document for MELPA (pastehub.el)

--- client/emacs/pastehub.el
+++ client/emacs/pastehub.el
@@ -25,12 +25,15 @@
 ;;; Commentary:
 
 ;; PasteHub.net Cloud-based cross-platform clipboard (copy and paste)
-;; service.  PasteHub.el is an emacs client for PasteHub.net .
+;; service.  PasteHub.el is an emacs client for PasteHub.net.
 ;;
 ;; PasteHub.el can synchronize kill-ring with PasteHub.net cloud
 ;; service automatically.  You don't have to learn new key-bindings.
 ;; 
-;; To get you started, add this line to your startup file:
+;; To get you started, you need to install PasteHub client for your client OS.
+;;  http://pastehub.net/
+;; 
+;; and then, add this line to your .emacs file:
 ;;  (require 'pastehub)
 ;;
 
